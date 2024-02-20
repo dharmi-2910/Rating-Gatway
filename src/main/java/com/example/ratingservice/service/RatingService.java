c94881126581fa1439ccf7eb18cfe9d28f8dd1e0
abc <abc2@gmail.com>
2024-02-20 18:14:26 +0530

    rating cometed

--- src/main/java/com/example/ratingservice/service/RatingService.java
+++ src/main/java/com/example/ratingservice/service/RatingService.java
@@ -4,7 +4,6 @@ import com.example.ratingservice.Entities.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
 
 @FeignClient(name="RATING-SERVICES")
