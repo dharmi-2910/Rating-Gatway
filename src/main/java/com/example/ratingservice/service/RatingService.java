fe4a5a565f4add762214a94b0265b93b8b28071d
abc <abc2@gmail.com>
2024-02-21 12:07:06 +0530

    rating cometed

--- src/main/java/com/example/ratingservice/service/RatingService.java
+++ src/main/java/com/example/ratingservice/service/RatingService.java
@@ -1,6 +1,5 @@
 package com.example.ratingservice.service;
 
-import com.example.ratingservice.Entities.Hotel;
 import com.example.ratingservice.Entities.Rating;
 import java.util.List;
 
@@ -9,7 +8,6 @@ public interface RatingService {
     Rating create(Rating rating);
     List<Rating> getAllRatings();
     List<Rating> getRatingByUserid(int userid);
-
     List<Rating> getRatingByHotelid(int hotelid);
     List<Rating> getRatingByRatingid(int ratingid);
 
